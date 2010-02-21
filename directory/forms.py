ef20ca298c529d0f3c0bd15964020824512bfa47
chase <chase@cuchulain.local>
2010-02-21 01:32:57 -0800

    formsets require at least one form

--- directory/forms.py
+++ directory/forms.py
@@ -3,10 +3,32 @@
 from datetime import date
 
 from django import forms
-from directory.models import Category, Business, Address, PhoneNumber, Owner
 from django.forms.models import inlineformset_factory, BaseInlineFormSet
-
 from django.contrib.admin import widgets
+from django.template import Context, loader
+
+from directory.models import Category, Business, Address, PhoneNumber, Owner
+
+class ExtraInlineFormset(BaseInlineFormSet):
+    """
+    """
+    min_num = 1
+    
+    def clean(self):
+        """
+        """
+        if len([d for d in self.cleaned_data if len(d) > 0]) < self.min_num:
+            raise forms.ValidationError("%s or more %s required" % (self.min_num, self.prefix))
+        
+        return super(ExtraInlineFormset, self).clean()
+    
+    def as_ul(self):
+        """
+        """
+        t = loader.get_template('formset_ul.html')
+        c = Context({'formset': self})
+        return t.render(c)
+
 
 class AddressForm(forms.ModelForm):
     do_not_publish_addy = forms.BooleanField(label="Do not publish address", required=False, initial=False)
@@ -31,15 +53,16 @@ class OwnerForm(forms.ModelForm):
                   'tribe',
                   'onaben_client',
                  )
+        
 
-AddressFormset = inlineformset_factory(Business, Address, 
-                                       form=AddressForm,
+AddressFormset = inlineformset_factory(Business, Address, form=AddressForm, 
+                                       formset=ExtraInlineFormset,
                                        extra=1, can_delete=False)
-PhoneFormset = inlineformset_factory(Business, PhoneNumber, 
-                                     form=PhoneForm, 
+PhoneFormset = inlineformset_factory(Business, PhoneNumber, form=PhoneForm, 
+                                     formset=ExtraInlineFormset,
                                      extra=2, can_delete=False)
-OwnerFormset = inlineformset_factory(Business, Owner, 
-                                     form=OwnerForm,
+OwnerFormset = inlineformset_factory(Business, Owner, form=OwnerForm, 
+                                     formset=ExtraInlineFormset,
                                      extra=1, can_delete=False)
 
 class BusinessForm(forms.ModelForm):
@@ -63,10 +86,7 @@ class BusinessForm(forms.ModelForm):
                   'home_based',
                  )
     
-    subform_types = {'owners': OwnerFormset, 
-                     'addresses': AddressFormset, 
-                     'phone_numbers': PhoneFormset,
-                    }
+    inlines = [OwnerFormset, AddressFormset, PhoneFormset, ]
     
     def clean_categories(self):
         data = self.cleaned_data['categories']
@@ -74,24 +94,24 @@ class BusinessForm(forms.ModelForm):
             raise forms.ValidationError("Please select no more than three categories")
 
         return data
-
     
     def __init__(self, *args, **kwargs):
-        self.subforms = {}
-        for key, formset in self.subform_types.items():
-            self.subforms[key] = formset(prefix=key, *args, **kwargs)
+        self.formsets = {}
+        for formset_class in self.inlines:
+            prefix = formset_class.get_default_prefix()
+            self.formsets[prefix] = formset_class(prefix=prefix, *args, **kwargs)
         return super(BusinessForm, self).__init__(*args, **kwargs)
     
     def is_valid(self, *args, **kwargs):
         valid =  super(BusinessForm, self).is_valid(*args, **kwargs)
-        for subform in self.subforms.values():
+        for subform in self.formsets.values():
             valid = valid and subform.is_valid(*args, **kwargs)
         return valid
         
     def save(self, *args, **kwargs):
         biz = super(BusinessForm, self).save(*args, **kwargs)
         
-        for subform in self.subforms.values():
+        for subform in self.formsets.values():
             subform.instance = biz
             subform.save(*args, **kwargs)
         
