46218c67fa81cdbf127a821d4ab1a51d22416981
chase <chase@cuchulain.local>
2010-04-03 14:30:51 -0700

    docstrings

--- utils/alphapaginator.py
+++ utils/alphapaginator.py
@@ -1,19 +1,28 @@
 """
+Alpha Paginator module
 """
 from django.core.paginator import Paginator
 from django.db import connection
 
 class AlphaPaginator(Paginator):
     """
+    Extention on Django core paginator to allow pagination by the first letter of
+    a specified field (which defaults to 'name') of the given model
     """
+    
     def __init__(self, *args, **kwargs):
         """
+        Override Paginator.__init__ to also take the parameter 'field'
+        @param field: The field by which to alphabetically organize the pages.  Defaults to 'name'
         """
         self.field = kwargs.get('field', 'name')
         super(AlphaPaginator, self).__init__(*args, **kwargs)
     
     def page(self, number):
         """
+        Override Paginator.page to accept strings
+        if the string isn't a number, it will take the first letter, and find
+        the first page with an object matching that letter
         """
         num_str = str(number)
         if num_str.isdigit():
@@ -27,6 +36,10 @@ class AlphaPaginator(Paginator):
         return super(AlphaPaginator, self).page(number)
     
     def alphabet(self):
+        """
+        @return: A list containing all of the first letters (capitalized) available
+        in the objects list.
+        """
         sql = """
         SELECT DISTINCT UPPER(SUBSTRING({field},1,1)) 
         FROM {table} order by {field};
