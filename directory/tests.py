442870acfbaa38cfa2dbbb25935822cead05963a
chase <chase@cuchulain.local>
2010-04-04 15:11:42 -0700

    test directory middleware

--- directory/tests.py
+++ directory/tests.py
@@ -2,6 +2,10 @@
 """
 from django.test import TestCase
 
+from directory.models import Category, Business
+from directory.middleware import popular_categories
+from django.conf import settings
+
 class TestForms(TestCase):
     """
     """
@@ -12,7 +16,25 @@ class TestMiddleware(TestCase):
     
     def test_popular_categories(self):
         """
+        Assert that popular_categories returns a correctly ordered list of the categories
+        with the most businesses attached, with biz_num set to the correct value
         """
+        cats = []
+        for c in range(30):
+            cat = Category.objects.create(name="test category %s" % c)
+            for b in range(c):
+                biz, created = Business.objects.get_or_create(name="test business %s" % b)
+                cat.businesses.add(biz)
+                cat.save()
+            if 30-settings.POPULAR_CATEGORY_COUNT <= c:
+                cats.insert(0, cat)
+        
+        context = popular_categories(None)
+        popcats = context['popcats']
+        self.assertEqual(settings.POPULAR_CATEGORY_COUNT, len(popcats))
+        for expected, found in zip(cats, popcats):
+            self.assertEqual(expected, found)
+            self.assertEqual(found.name, "test category %s" % found.biz_num, )
 
 class FunctionalTests(TestCase):
     """
