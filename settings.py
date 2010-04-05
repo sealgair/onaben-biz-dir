90d5f5648e6dec7c026f6e7f54463b5099dc9b4f
chase <chase@cuchulain.local>
2010-04-04 17:25:43 -0700

    turn debug off

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Django settings for onaben project.
 import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -17,8 +17,6 @@ DATABASE_NAME = 'onaben'       # Or path to database file if using sqlite3.
 DATABASE_PASSWORD = ''     # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
